2f4e8166a1e50920c6498a24c8d502c343e7a731
Patrick Robin <patrick-robin@gmx.net>
2020-11-22 22:55:30 +0100

    Added the CarRefuelTrackerModule

---
+++ Modules/RietRobHaushaltsbuch.Modules.CarRefuelTracker/CarRefuelTrackerModule.cs
@@ -0,0 +1,20 @@
+﻿using RietRobHaushaltsbuch.Modules.CarRefuelTracker.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace RietRobHaushaltsbuch.Modules.CarRefuelTracker
+{
+    public class CarRefuelTrackerModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
