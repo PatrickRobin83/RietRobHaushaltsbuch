489cf4b80e2243d0316024948163e269627ef88f
Immersion7\p.robin <p.robin@smartperform.de>
2020-12-07 16:03:15 +0100

    Added the View and the ViewModel for the GarbageTracker

--- Modules/RietRobHaushaltsbuch.Modules.GarbageTracker/GarbageTrackerModule.cs
+++ Modules/RietRobHaushaltsbuch.Modules.GarbageTracker/GarbageTrackerModule.cs
@@ -1,14 +1,17 @@
 ﻿using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Mvvm;
 using Prism.Regions;
+using RietRobHaushaltbuch.Core;
+using RietRobHaushaltsbuch.Modules.GarbageTracker.ViewModels;
+using RietRobHaushaltsbuch.Modules.GarbageTracker.Views;
 
 namespace RietRobHaushaltsbuch.Modules.GarbageTracker
 {
     public class GarbageTrackerModule : IModule
     {
         private readonly IRegionManager _regionManager;
-
-
+        
         public GarbageTrackerModule(IRegionManager regionManager)
         {
             _regionManager = regionManager;
@@ -16,12 +19,12 @@ namespace RietRobHaushaltsbuch.Modules.GarbageTracker
 
         public void OnInitialized(IContainerProvider containerProvider)
         {
- 
+            _regionManager.RegisterViewWithRegion(RegionNames.MainRegion, typeof(GarbageTrackerOverView));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            
+            ViewModelLocationProvider.Register<GarbageTrackerOverView, GarbageTrackerOverViewModel>();
         }
     }
 }
